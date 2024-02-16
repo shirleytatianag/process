f55d7239be72ed028b9efd2c89eeb1e79608c1ab
Shirley Tatiana García <shirleygarcia@wposs.con>
2024-02-16 11:07:32 -0500

    Primera sección de la landing-page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {LandingPageComponent} from "./shared/layouts/landing-page/landing-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  }
+];
